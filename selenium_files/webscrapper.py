36af6470b17d03b8400832a4260bf3cfb2b76e28
unknown <ali.haider.7000@gmail.com>
2022-05-31 16:55:57 +0500

    Script updates

--- selenium_files/webscrapper.py
+++ selenium_files/webscrapper.py
@@ -3,31 +3,57 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
+import urllib
 
-PATH = r"C:\Users\Nexus\Documents\Selenium Webscrapper\selenium_files\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://images.google.com/")
-print(driver.title)
-
+query = "Cars"
 search_url_element = driver.find_element(by=By.NAME, value="q")
-search_url_element.send_keys("Cars")
+search_url_element.send_keys(query)
 search_url_element.send_keys(Keys.RETURN)
 
+def scroll_to_bottom():
+ 
+    last_height = driver.execute_script('\
+    return document.body.scrollHeight')
+ 
+    while True:
+        driver.execute_script('\
+        window.scrollTo(0,document.body.scrollHeight)')
+        time.sleep(3)
+ 
+        new_height = driver.execute_script('\
+        return document.body.scrollHeight')
+ 
+        # Click on "Show more results"
+        try:
+            driver.find_element_by_css_selector(".YstHxe input").click()
+            time.sleep(3)
+        except:
+            pass
+ 
+        if new_height == last_height:
+            break
+        last_height = new_height
 
+# element = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.CLASS_NAME, "OztcRd"))
+# )
 
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "OztcRd"))
-    )
+scroll_to_bottom()
+time.sleep(2)
 
-    names = driver.find_elements_by_class_name("OztcRd")
-    for name in names:
-        print(name.text)
-finally:
-    # driver.quit()
-    pass
+for i in range(50):
+    try:
+        image = driver.find_element(By.XPATH, '//*[@id="islrg"]/div[1]/div['+str(i)+']/a[1]/div[1]/img')
+        image.screenshot("C:/Users/Nexus/Documents/Selenium Webscrapper/webscraper_results/" + query + str(i) + ".png")
+    except:
+        continue
+
 
 
 # time.sleep(5)
