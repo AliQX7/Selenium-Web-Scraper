d6466a7cd94f14db23bb06714faf25c2138dbc97
Ali <ali.haider.7000@gmail.com>
2022-06-22 22:15:50 +0500

    Updates

--- selenium_files/webscrapper.py
+++ selenium_files/webscrapper.py
@@ -45,6 +45,7 @@ def scroll_to_height(amount):
     driver.execute_script('\
         window.scrollTo(0,{})'.format(amount))
 
+
 scroll_to_height(700)
 one_thousandCC_cars_element = driver.find_element(By.XPATH, '//*[@id="browesCTGSlider"]/div[1]/ul[1]/li[1]/a/img')
 two_thousandCC_cars_element = driver.find_element(By.XPATH, '//*[@id="browesCTGSlider"]/div[1]/ul[1]/li[2]/a/img')
@@ -55,16 +56,16 @@ for i in range(50):
     ad_titles.append(driver.find_element(By.XPATH, '//*[@id="main_ad_6255096"]/div/div[2]/div[1]/div/div/a/h3'))
     '//*[@id="main_ad_6203273"]/div/div[2]/div[1]/div/div/a/h3'
 
-# element = WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located((By.CLASS_NAME, "OztcRd"))
-# )
+element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "OztcRd"))
+)
 
-# Parse through the first 50 images on the webpage and save them as screenshots.
-# for i in range(50):
-#     try:
-#         image = driver.find_element(By.XPATH, '//*[@id="islrg"]/div[1]/div['+str(i)+']/a[1]/div[1]/img')
-#         image.screenshot("C:/Users/Nexus/Documents/Selenium Webscrapper/webscraper_results/" + query + "_" + str(i) + ".png")
-#     except:
-#         continue
+Parse through the first 50 images on the webpage and save them as screenshots.
+for i in range(50):
+    try:
+        image = driver.find_element(By.XPATH, '//*[@id="islrg"]/div[1]/div['+str(i)+']/a[1]/div[1]/img')
+        image.screenshot("C:/Users/Nexus/Documents/Selenium Webscrapper/webscraper_results/" + query + "_" + str(i) + ".png")
+    except:
+        continue
 
 
